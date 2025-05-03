9c7d7de4e171873879f339d98281de555bcf5edc
jul <jul@gmail.com>
2025-05-03 12:37:27 +0200

    carga a github

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/buscador.css'
+import { BuscadorPeliculas } from './BuscadorPeliculas.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <BuscadorPeliculas />
+  </StrictMode>,
+)
